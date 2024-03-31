8b3f686ab46387628336b04ee461730d9f1cbd3b
BrunoFreitas <brunods.freitas@hotmail.com>
2024-03-31 14:05:19 -0300

    (test) ci

--- ci-login.sh
+++ ci-login.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Tentativa de Login DockerHub"
-echo "$DOCKER_USERNAME" | docker login -u "$DOCKER_TOKEN" --password-stdin > /dev/null
+echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin > /dev/null
 
